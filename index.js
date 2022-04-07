ab83dca44c90abedb88cd40af80a32b9aa0f922c
carloscastillo10 <carloscastillo090916@gmail.com>
2022-04-07 12:52:23 -0500

    :sparkles: Adding sequelize orm error handler middleware

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-const express = require("express");
-const cors = require("cors"); // Para gestionar a quien se le da acceso a la aplicacion
-const routerApi = require("./routes");
+const express = require('express');
+const cors = require('cors'); // Para gestionar a quien se le da acceso a la aplicacion
+const routerApi = require('./routes');
 const {
     logErrors,
     errorHandler,
     boomErrorHandler,
-} = require("././middlewares/error.handler");
+    ormErrorHandler,
+} = require('././middlewares/error.handler');
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middelware
 app.use(express.json());
 
-const whitelist = ["http://127.0.0.1:5500", "https://myapp.com"];
+const whitelist = ['http://127.0.0.1:5500', 'https://myapp.com'];
 const options = {
     origin: (origin, callback) => {
         if (whitelist.includes(origin) || !origin) {
             callback(null, true);
         } else {
-            callback(new Error("No permitido"));
+            callback(new Error('No permitido'));
         }
     },
 };
 app.use(cors(options)); // Resolver problemas
 
-app.get("/", (req, res) => {
-    res.send("Hello my server in express");
+app.get('/', (req, res) => {
+    res.send('Hello my server in express');
 });
 
-app.get("/new-route", (req, res) => {
+app.get('/new-route', (req, res) => {
     res.send("Hello, i'm new route");
 });
 
@@ -37,9 +38,10 @@ routerApi(app);
 // Los middlewares de tipo error se deben utilizar despues de definir el router
 // Se ejecutan en el orden que se los coloque
 app.use(logErrors);
+app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log("My port " + port);
+    console.log('My port ' + port);
 });
