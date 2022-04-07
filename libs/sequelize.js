a1dd881afc98a36eee85802e846977eb0254d7da
carloscastillo10 <carloscastillo090916@gmail.com>
2022-04-07 18:55:36 -0500

    :sparkles: Adding customer router in routing

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -4,15 +4,15 @@ const setUpModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser); // Proteger la informacion
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-    dialect: 'mysql',
+    dialect: 'postgres',
     loggin: true, // Mostrar el resultado en comando directo en sql
 });
 
 setUpModels(sequelize); // Enviarle la conexión
 
-sequelize.sync() // Crear la estructura en la base de datos
+// sequelize.sync() // Crear la estructura en la base de datos
 
 module.exports = sequelize;
