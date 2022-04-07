fae98e235222fd3eb06c5f24efefea97733de4e7
carloscastillo10 <carloscastillo090916@gmail.com>
2022-04-07 16:18:07 -0500

    :wrench: Connect sequelize orm to mysql

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -4,10 +4,10 @@ const setUpModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser); // Proteger la informacion
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-    dialect: 'postgres',
+    dialect: 'mysql',
     loggin: true, // Mostrar el resultado en comando directo en sql
 });
 
