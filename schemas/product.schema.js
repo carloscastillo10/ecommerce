ed65e73ab0cc64958c34ae048eeebf432db12d1f
carloscastillo10 <carloscastillo090916@gmail.com>
2022-04-06 20:29:49 -0500

    :sparkles: Adding api schemas

---
+++ schemas/product.schema.js
@@ -0,0 +1,25 @@
+// https://joi.dev/api/?v=17.6.0
+const Joi = require("joi"); // Validar la data que se envia desde el cliente
+
+const id = Joi.string().uuid();
+const name = Joi.string().min(3).max(15);
+const price = Joi.number().integer().min(10);
+const image = Joi.string().uri();
+
+const createProductShema = Joi.object({
+    name: name.required(),
+    price: price.required(),
+    image: image.required(),
+});
+
+const updateProductShema = Joi.object({
+    name: name,
+    price: price,
+    image: image,
+});
+
+const getProductShema = Joi.object({
+    id: id.required(),
+});
+
+module.exports = { createProductShema, updateProductShema, getProductShema };
