f0475bad1b4bd071ac8aa7404f4a00aa798ad7ab
carloscastillo10 <carloscastillo090916@gmail.com>
2022-04-07 18:54:10 -0500

    :hammer: Updte user model

--- db/models/index.js
+++ db/models/index.js
@@ -1,9 +1,18 @@
-// Encargado de enviar la coneccion hacia los modelos
-
 const { User, UserSchema } = require('./user.model');
+const { Customer, CustomerSchema } = require('./customer.model');
+const { Category, CategorySchema } = require('./category.model');
+const { Product, ProductSchema } = require('./product.model');
 
-function setUpModels(sequelize) {
+function setupModels(sequelize) {
     User.init(UserSchema, User.config(sequelize));
+    Customer.init(CustomerSchema, Customer.config(sequelize));
+    Category.init(CategorySchema, Category.config(sequelize));
+    Product.init(ProductSchema, Product.config(sequelize));
+
+    User.associate(sequelize.models);
+    Customer.associate(sequelize.models);
+    Category.associate(sequelize.models);
+    Product.associate(sequelize.models);
 }
 
-module.exports = setUpModels;
+module.exports = setupModels;
--- db/models/user.model.js
+++ db/models/user.model.js
@@ -16,6 +16,11 @@ const UserSchema = {
         allowNull: false,
         type: DataTypes.STRING,
     },
+    role: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        defaultValue: 'customer',
+    },
     createdAt: {
         allowNull: false,
         type: DataTypes.DATE,
@@ -25,7 +30,12 @@ const UserSchema = {
 };
 
 class User extends Model {
-    static associate() {}
+    static associate(models) {
+        this.hasOne(models.Customer, {
+            as: 'customer',
+            foreignKey: 'userId'
+        });
+    }
 
     static config(sequelize) {
         return {
