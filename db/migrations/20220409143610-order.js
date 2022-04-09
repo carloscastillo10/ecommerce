9bdf4b0981c4e01cfe54e6ea47f7164f31ef0fde
carloscastillo10 <carloscastillo090916@gmail.com>
2022-04-09 12:55:14 -0500

    :zip: Refactor production migrations

--- db/migrations/20220409143610-order.js
+++ db/migrations/20220409143610-order.js
@@ -1,10 +1,33 @@
 'use strict';
 
-const { OrderSchema, ORDER_TABLE } = require('./../models/order.model');
+const { DataTypes, Sequelize } = require('sequelize');
+const { ORDER_TABLE } = require('./../models/order.model');
 
 module.exports = {
     async up(queryInterface) {
-        await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+        await queryInterface.createTable(ORDER_TABLE, {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: DataTypes.INTEGER,
+            },
+            name: {
+                type: DataTypes.STRING,
+                unique: true,
+                allowNull: false,
+            },
+            image: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            createdAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+                field: 'created_at',
+                defaultValue: Sequelize.NOW,
+            },
+        });
     },
 
     async down(queryInterface) {
