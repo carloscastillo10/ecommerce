d196625c16c11fe532a7a95102a6fa6622de8e65
carloscastillo10 <carloscastillo090916@gmail.com>
2022-04-25 20:39:14 -0500

    :zip: Refactor auth logic

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+
 const { config } = require('./../config/config');
+const AuthService = require('./../services/auth.service');
+
 const router = express.Router();
+const service = new AuthService();
 
 router.post(
     '/login',
@@ -10,20 +14,19 @@ router.post(
     async (req, res, next) => {
         try {
             const user = req.user;
-            const payload = {
-                sub: user.id,
-                role: user.role,
-            };
-            const token = jwt.sign(payload, config.jwtSecret);
-
-            res.json({
-                user,
-                token,
-            });
+            res.json(service.singToken(user));
         } catch (error) {
             next(error);
         }
     }
 );
+
+router.post('/recovery', async (req, res, next) => {
+    try {
+        const { email } = req.body;
+    } catch (error) {
+        next(error);
+    }
+});
 
 module.exports = router;
---
+++ services/auth.service.js
@@ -0,0 +1,38 @@
+const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { config } = require('./../config/config');
+const UserService = require('./user.service');
+const service = new UserService();
+
+class AuthService {
+    async getUser(email, password) {
+        const user = await service.findByEmail(email);
+        if (!user) {
+            throw (boom.unauthorized(), false);
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            throw (boom.unauthorized(), false);
+        }
+        delete user.dataValues.password;
+        return user;
+    }
+
+    singToken(user) {
+        const payload = {
+            sub: user.id,
+            role: user.role,
+        };
+        const token = jwt.sign(payload, config.jwtSecret);
+
+        return {
+            user,
+            token,
+        };
+    }
+}
+
+module.exports = AuthService;
